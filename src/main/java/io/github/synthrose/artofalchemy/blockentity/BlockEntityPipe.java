9bca8611f22534f2e1ddfa009bb47f11f4c6cb4b
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-08-19 17:39:54 +0200

    BlockEntityPipe: Expose IOFace configuration as render data

--- src/main/java/io/github/synthrose/artofalchemy/blockentity/BlockEntityPipe.java
+++ src/main/java/io/github/synthrose/artofalchemy/blockentity/BlockEntityPipe.java
@@ -2,17 +2,21 @@ package io.github.synthrose.artofalchemy.blockentity;
 
 import io.github.synthrose.artofalchemy.transport.NetworkNode;
 import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
+import net.fabricmc.fabric.api.rendering.data.v1.RenderAttachedBlockView;
+import net.fabricmc.fabric.api.rendering.data.v1.RenderAttachmentBlockEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockRenderView;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class BlockEntityPipe extends BlockEntity implements BlockEntityClientSerializable {
+public class BlockEntityPipe extends BlockEntity implements BlockEntityClientSerializable, RenderAttachmentBlockEntity {
 
     private Map<Direction, IOFace> faces = new HashMap<>();
 
@@ -67,6 +71,24 @@ public class BlockEntityPipe extends BlockEntity implements BlockEntityClientSer
     @Override
     public void sync() {
         BlockEntityClientSerializable.super.sync();
+    }
+
+    @Override
+    public Object getRenderAttachmentData() {
+        assert Direction.values().length == 6;
+        IOFace[] faceConfig = new IOFace[6];
+        for (final Map.Entry<Direction, IOFace> entry : faces.entrySet()) {
+            faceConfig[entry.getKey().ordinal()] = entry.getValue();
+        }
+        return faceConfig;
+    }
+
+    // Convenience function which performs the correct downcast of the Object returned
+    // in getRenderAttachmentData()
+    public static IOFace[] getRenderAttachedFaceConfig(BlockRenderView blockRenderView, BlockPos blockPos) {
+        RenderAttachedBlockView renderAttachedBlockView = ((RenderAttachedBlockView) blockRenderView);
+        Object renderAttachment = renderAttachedBlockView.getBlockEntityRenderAttachment(blockPos);
+        return (IOFace[]) renderAttachment;
     }
 
     public enum IOFace implements StringIdentifiable {
