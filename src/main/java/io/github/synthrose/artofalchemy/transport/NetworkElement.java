3b8516dd74f6132803cdcfbc5afdad6fae5ffff6
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-10-18 04:15:41 +0200

    NetworkElement: Add partial javadoc

--- src/main/java/io/github/synthrose/artofalchemy/transport/NetworkElement.java
+++ src/main/java/io/github/synthrose/artofalchemy/transport/NetworkElement.java
@@ -9,7 +9,37 @@ import java.util.Set;
 public interface NetworkElement {
 	boolean hasNodes(World world, BlockPos pos);
 	Set<NetworkNode> getNodes(World world, BlockPos pos);
+
+	/**
+	 * Query whether this element is connected to a neighbouring element.
+	 *
+	 * @param world the world instance.
+	 * @param pos the block position identifying this element.
+	 * @param dir the direction in which the other element is located.
+	 *
+	 * @return whether the two elements are connected.
+	 */
 	boolean isConnected(World world, BlockPos pos, Direction dir);
+
+	/**
+	 * Query whether this element is connected to another element.
+	 *
+	 * @param world the world instance.
+	 * @param pos the block position identifying this element.
+	 * @param other the block position identifying the other element.
+	 *
+	 * @return whether the two elements are connected.
+	 */
 	boolean isConnected(World world, BlockPos pos, BlockPos other);
+
+	/**
+	 * Query the set of surrounding blocks that have an established
+	 * connection to this element.
+	 *
+	 * @param world the world instance.
+	 * @param pos the block position identifying this element.
+	 *
+	 * @return the set of block positions towards which a connection is established.
+	 */
 	Set<BlockPos> getConnections(World world, BlockPos pos);
 }
