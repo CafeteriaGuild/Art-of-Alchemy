3b2e0c1420fa2b05218feb6f8abc12b4de3f73e2
Kyran Gostelow <kyran@gostelow.me>
2021-10-01 00:30:53 +1000

    TagRegistry -> TagFactory

--- src/main/java/dev/cafeteria/artofalchemy/blockentity/BlockEntityCalcinator.java
+++ src/main/java/dev/cafeteria/artofalchemy/blockentity/BlockEntityCalcinator.java
@@ -13,7 +13,7 @@ import dev.cafeteria.artofalchemy.util.ImplementedInventory;
 import io.github.cottonmc.cotton.gui.PropertyDelegateHolder;
 import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
 import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
@@ -320,7 +320,7 @@ PropertyDelegateHolder, BlockEntityClientSerializable, SidedInventory, ExtendedS
 	@Override
 	public boolean canExtract(int slot, ItemStack stack, Direction dir) {
 		if (dir == Direction.DOWN && slot == 0) {
-			return TagRegistry.item(ArtOfAlchemy.id("containers")).contains(stack.getItem());
+			return TagFactory.ITEM.create(ArtOfAlchemy.id("containers")).contains(stack.getItem());
 		} else {
 			return true;
 		}
--- src/main/java/dev/cafeteria/artofalchemy/util/AoATags.java
+++ src/main/java/dev/cafeteria/artofalchemy/util/AoATags.java
@@ -2,12 +2,12 @@ package dev.cafeteria.artofalchemy.util;
 
 import dev.cafeteria.artofalchemy.ArtOfAlchemy;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 
 public class AoATags {
-	public static final Tag<Item> CONTAINERS = TagRegistry.item(ArtOfAlchemy.id("containers"));
+	public static final Tag<Item> CONTAINERS = TagFactory.ITEM.create(ArtOfAlchemy.id("containers"));
 
 	public static void init() { }
 }
