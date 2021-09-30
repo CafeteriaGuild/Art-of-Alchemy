d6ec06202e6eb4fa52b0dd485b712e846605f01d
Kyran Gostelow <kyran@gostelow.me>
2021-10-01 00:54:46 +1000

    Mod Menu

--- src/main/java/dev/cafeteria/artofalchemy/integration/AoAxModMenu.java
+++ src/main/java/dev/cafeteria/artofalchemy/integration/AoAxModMenu.java
@@ -2,8 +2,8 @@ package dev.cafeteria.artofalchemy.integration;
 
 import dev.cafeteria.artofalchemy.AoAConfig;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -11,7 +11,6 @@ import net.fabricmc.api.Environment;
 @Environment(EnvType.CLIENT)
 public class AoAxModMenu implements ModMenuApi {
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
 		return (screen) -> AutoConfig.getConfigScreen(AoAConfig.class, screen).get();
